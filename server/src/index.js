37ccc9bbf4ff1165dbfddebb837782e4349449bd
c0950760 <c0950760@mylambton.ca>
2025-12-11 18:42:05 -0500

    basic structure

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+
+// src/index.js
+const express = require("express");   // or  if using CommonJS // or if using ES module import express from "express"
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
