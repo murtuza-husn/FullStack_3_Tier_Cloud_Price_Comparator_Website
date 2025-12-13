a3fbfcf1e6a84e2a94d24d6521b4cda4c5860efc
krathva707 <c0947900@mylambton.ca>
2025-12-12 20:27:13 -0500

    app port listen

--- server/server.js
+++ server/server.js
@@ -25,9 +25,12 @@ app.use("/api/pricing", pricingRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => 
+      console.log(`Server running on http://0.0.0.0:${PORT}`)
+    );
   })
   .catch(err => console.error(err));
+
 
 
   
--- server/src/index.js
+++ server/src/index.js
@@ -40,6 +40,7 @@ app.post("/api/pricing", (req, res) => {
   res.json(pricingData);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
