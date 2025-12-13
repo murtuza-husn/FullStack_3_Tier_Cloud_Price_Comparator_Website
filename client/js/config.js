53b8b2c3fe87e28123000cffa50d9d2b1fe61405
krathva707 <c0947900@mylambton.ca>
2025-12-12 20:12:16 -0500

    config.js change

--- client/js/config.js
+++ client/js/config.js
@@ -2,4 +2,4 @@
 // Central config for client-side API base URL
 // Use this in all API calls
 
-const BASE_URL = 'http://localhost:5000'; // Will be replaced by env or build tools in production
+const BASE_URL = "http://3.139.63.249:5000"; // Will be replaced by env or build tools in production
